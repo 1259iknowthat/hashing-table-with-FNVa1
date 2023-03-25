67057a810f67820fd6d5b0e95a1ec854cb7ba70e
1259iknowthat <89141562+1259iknowthat@users.noreply.github.com>
2023-03-26 00:36:36 +0700

    update source code

--- hash-table.cpp
+++ hash-table.cpp
@@ -3,54 +3,53 @@
 
 using namespace std;
 
-const int TABLE_SIZE = 100000;
-vector<pair<unsigned int, int>> table(TABLE_SIZE, make_pair(0, -1));
+const int TABLE_SIZE_M = 100000;
+vector<pair<unsigned int, int>> table(TABLE_SIZE_M, make_pair(0, -1));
 
 class FNV1a_Hash{
     private:
         string key;
         const int FNV_PRIME = 16777619;
         const int OFFSET_BASIS = 2166136261;
+        unsigned int value;
 
     public:
         unsigned int hashingFunc(string key) {
             unsigned int hash = OFFSET_BASIS;
             for (char c : key) {
-                hash ^= static_cast<unsigned int>(c);
+                hash ^= static_cast<unsigned int>(c); // xor with every elements in binary string
                 hash *= FNV_PRIME;
             }
             return hash;
         }
 
         void insertValue(string key){
-            unsigned int value = hashingFunc(key);
-            int index = 0 % TABLE_SIZE;
+            this->value = hashingFunc(key);
+            unsigned int index = 0 % TABLE_SIZE_M;
             // handle collision
             while (table[index].second != -1) {
-                index = (index + 1) % TABLE_SIZE;
+                index = (index + 1) % TABLE_SIZE_M;
             }
-            table[index].first = value;
+            table[index].first = this->value;
             table[index].second = 10; // signature for detecting collision
         }
 
         void getValue(string key){
-            unsigned int value = hashingFunc(key);
-            int index = 0 % TABLE_SIZE;
+            this->value = hashingFunc(key);
+            unsigned int index = 0 % TABLE_SIZE_M;
             while (table[index].second != -1 && table[index].first != value) {
-                index = (index + 1) % TABLE_SIZE;
+                index = (index + 1) % TABLE_SIZE_M;
             }
             value = table[index].first;
             if (value <= 0) {
-                cout << "Key not found!" << endl;
+                puts("Key not found!");
             } else {
-                printf("Hash value: %u\n", value);
+                printf("Hash value: %u\n", this->value);
                 printf("Index in table: %u\n", index);
             }
         }
 
-        FNV1a_Hash(){
-            this->key = "00000000";
-        };
+        FNV1a_Hash(){};
         FNV1a_Hash(string key){
             this->key = key;
             this->insertValue(this->key);
